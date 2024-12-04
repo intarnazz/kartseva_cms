6bb92c7728db0f542830e7f975d91ff2bb6a3812
ortadox <misha.vidiakin@gmail.com>
2024-12-04 09:50:34 +0300

    init

---
+++ src/js/script.js
@@ -0,0 +1,141 @@
+let obj = [];
+
+if (localStorage.getItem("obj") !== null) {
+  obj = JSON.parse(localStorage.getItem("obj"));
+  console.log(obj);
+}
+
+import delRow from "../modules/delPage.js";
+import addProduct from "../modules/addElPage.js";
+import * as change from "../modules/changePage.js";
+{
+  
+  const getFromPage = () => {
+    return {
+      table: document.querySelector(".cms-main__container__table-form__table"),
+      sumValue: document.querySelector(".cms-main__head__sum__value-price"),
+      addBtnMain: document.querySelector(
+        ".cms-main__container__form__add-button"
+      ),
+      formAddItem: document.querySelector(".add-item"),
+      formClose: document.querySelector(".cancel"),
+      inputForm: document.querySelectorAll(".add-item__main-section__form"),
+      addProductBtn: document.querySelector(".add-product"),
+      allForm: document.querySelector(".add-item__main-section"),
+      checkbox: document.querySelector(".add-item__main-section__checkbox"),
+      input: document.querySelector(".add-item__main-section__form.litle"),
+      checkmark: document.querySelector('.checkmark'),
+      inputs: document.querySelectorAll('.add-item__main-section__form'),
+      tableId: document.querySelectorAll(
+        ".cms-main__container__table-form__table__row__col-id"
+      ),
+      sum: document.querySelectorAll('.cms-main__container__table-form__table__row__col-sum'),
+      addBtnMod: document.querySelector(".add-product"),
+    };
+  };
+
+
+
+  const init = () => {
+    let id = [];
+    let allConst = getFromPage();
+    addProduct(obj, id, allConst);
+    allConst = getFromPage();
+    change.changeValueEl(obj);
+    allConst.sumAllProduct = change.changeItog(allConst.sum);
+    change.changeId(allConst, obj, id);
+
+    allConst.allForm.addEventListener("submit", (e) => {
+      e.preventDefault();
+      const formData = new FormData(allConst.allForm);
+      const namee = formData.get("name");
+      const category = formData.get("category");
+      const description = formData.get("description");
+      const units = formData.get("units");
+      const discount = formData.get("discountt");
+      const count = formData.get("count");
+      const price = formData.get("price");
+
+      obj.push({
+        id: Math.floor(Math.random() * 100000),
+        denomination: namee,
+        category: category,
+        points: units,
+        count: count,
+        price: price,
+        get value() {
+          if (discount) {
+            return (this.count * this.price) - ((this.count * this.price) * (discount / 100));
+          } else {
+            return this.count * this.price;
+          }
+        },
+        discount: discount,
+      });
+
+      allConst.formAddItem.style.display = "none";
+      document.getElementById("modalSum").textContent = "$";
+      allConst.allForm.reset();
+      addProduct(obj, id, allConst);
+      change.changeValueEl(obj);
+      allConst = getFromPage();
+      allConst.sumAllProduct = change.changeItog(allConst.sum);
+      change.changeId(allConst, obj, id);
+    });
+
+    // Модальные окна
+    allConst.addBtnMain.addEventListener("click", () => {
+      allConst.formAddItem.style.display = "flex";
+    });
+    allConst.formClose.addEventListener("click", () => {
+      allConst.formAddItem.style.display = "none";
+    });
+
+    // Дисконт
+    allConst.checkbox.addEventListener("change", () => {
+      if (allConst.checkbox.checked) {
+        allConst.input.removeAttribute("disabled");
+      } else {
+        allConst.input.setAttribute("disabled", "");
+        allConst.input.value = "";
+      }
+    });
+
+    // Рассчёт в форме
+    allConst.inputs.forEach(elem => {
+      elem.addEventListener('change', () => {
+        console.log('HAHA')
+        const formData = new FormData(allConst.allForm);
+        const discount = formData.get("discountt");
+        const count = formData.get("count");
+        const price = formData.get("price");
+        document.getElementById('modalSum').textContent = '$ ' + change.calculateSum(count, price, discount)
+      })
+    });
+    allConst.checkmark.addEventListener('click', () => {
+      console.log('HAHA')
+      const formData = new FormData(allConst.allForm);
+      const discount = 0;
+      const count = formData.get("count");
+      const price = formData.get("price");
+      document.getElementById('modalSum').textContent = '$ ' + change.calculateSum(count, price, discount)
+    })
+
+
+    allConst.table.addEventListener("click", (e) => {
+      if (
+        e.target.classList.contains(
+          "cms-main__container__table-form__table__row__col-delete__icon"
+        )
+      ) {
+        delRow(obj, allConst, e);
+        change.changeValueEl(obj);
+        allConst = getFromPage();
+        allConst.sumAllProduct = change.changeItog(allConst.sum);
+        change.changeId(allConst, obj, id);
+      }
+    });
+  };
+
+  init();
+}
